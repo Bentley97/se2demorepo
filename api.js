e5c86d2ffff56f0a6d83f52d2db16540043dc878
Bentley97 <39089688+Bentley97@users.noreply.github.com>
2018-12-15 16:53:17 +0100

    Update app.js - 7

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
  
-var courses_offered = [{id: 21, name: 'HCI'}, {id: 28, name:'sweng'}, {id: 53, name: 'LFC'}]
+var courses_offered = [{id: 21, name: 'HCI'}, {id: 28, name:'sweng'}, {id: 50, name: 'LFC'}]
 
 app.get('/', (req, res) => res.send('Hello Prova!!!'))
  
